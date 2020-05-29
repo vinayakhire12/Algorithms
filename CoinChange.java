08fd89d27916454a1c032c4ab985d7b6d88650ad
vinayakhire12 <47803600+vinayakhire12@users.noreply.github.com>
2020-05-29 10:38:11 -0700

    CoinChange.java

---
+++ CoinChange.java
@@ -0,0 +1,21 @@
+You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+Input: coins = [1, 2, 5], amount = 11
+Output: 3
+
+
+public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        
+        for(int i = 0; i <= amount; i++){
+            for(int j = 0; j < coins; j++){
+                if(coins[j] <= i){
+                    dp[i] = Math.min(dp[i], 1 + dp[i - coins[j]]);
+                }
+            }
+        }
+        
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
